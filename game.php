d1985a98b5a0770b2890279fd07b5153d73201fa
megoel <mehul31@live.com>
2014-11-22 01:39:26 -0800

    game home page
    pahlease work

---
+++ game.php
@@ -0,0 +1,73 @@
+<?php
+    include 'connect.php';
+    $player1 = "SELECT * from players where name = 'player1'";
+    $player2 = mysql_query($player1) or die ("could not select players");
+    $player3 = mysql_fetch_array($player2);
+    $playerName = "You are logged in as " . $player3['name']; 
+
+    //echo "You are logged in as " .$player3['name'];
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crash Cash</title>
+
+    <!-- Bootstrap -->
+    <link href="CSS/bootstrap.min.css" rel="stylesheet">
+    <link href="CSS/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Crash Cash</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Stock Market</a></li>
+            <li><a href="#">Education</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Personal Information <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Manage your income</a></li>
+                <li><a href="#">Rent</a></li>
+                <li><a href="#">Taxes</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-right" role="search">
+            <button type="submit" class="btn btn-default">Logout</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="register.php"><input type="hidden" name="type" value="<?php echo $playerName; ?>" ></a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav><!-- End of Navbar -->
+
+    </body>
+
+  
+
+
+
